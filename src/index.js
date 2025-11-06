4176564abd319394e0dcf6f1264e8c4416ff05ed
Shivam Mishra <shivam.m150@hdfclife.com>
2025-11-06 12:10:53 +0530

    Restructure: Move all files to root directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on porttt ${PORT}`);
 });
